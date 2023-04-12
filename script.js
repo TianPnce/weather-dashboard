47b0b5a448738bdc87767983f7ff4a8fa2aa0c1a
TianPnce <122128252+TianPnce@users.noreply.github.com>
2023-04-11 23:15:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+var searchHistory = [];
+function getItems() {
+  var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
+    if (storedCities !== null) {
+    searchHistory = storedCities;
+  };
+  for (i = 0; i < searchHistory.length; i++) {
+    if (i == 8) {
+        break;
+      }
+    cityListButton = $("<a>").attr({
+      class: "list-group-item list-group-item-action",
+        ref: "#"
+    });
+    cityListButton.text(searchHistory[i]);
+    $(".list-group").append(cityListButton);
+  }
+};
+var city;
+var mainCard = $(".card-body");
+getItems();
+function getData() {
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=49fb27317373bb54f7d9243387af6df3" 
+  mainCard.empty();
+  $("#weeklyForecast").empty();
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).then(function (response) {
+    var date = moment().format(" MM/DD/YYYY");
+    var iconCode = response.weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+    var name = $("<h3>").html(city + date);
+    mainCard.prepend(name);
+    mainCard.append($("<img>").attr("src", iconURL));
+    var temp = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+    mainCard.append($("<p>").html("Temperature: " + temp + " &#8457"));
+    var humidity = response.main.humidity;
+    mainCard.append($("<p>").html("Humidity: " + humidity));
+    var windSpeed = response.wind.speed;
+    mainCard.append($("<p>").html("Wind Speed: " + windSpeed));
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/uvi?appid=382da5a44ba41c4495d001d13df120e7" + lat + "&lon=" + lon,
+    method: "GET"
+  }).then(function (response) {
+    mainCard.append($("<p>").html("UV Index: <span>" + response.value + "</span>"));
+    if (response.value <= 2) {
+      $("span").attr("class", "btn btn-outline-success");
+    };
+    if (response.value > 2 && response.value <= 5) {
+      $("span").attr("class", "btn btn-outline-warning");
+    };
+    if (response.value > 5) {
+      $("span").attr("class", "btn btn-outline-danger");
+    };
+      })
+
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=49fb27317373bb54f7d9243387af6df3",
+  }).then(function (response) {
+    for (i = 0; i < 5; i++) {
+      var newCard = $("<div>").attr("class", "col fiveDay bg-primary text-white rounded-lg p-2")
+      $("#weeklyForecast").append(newCard);
+      var myDate = new Date(response.list[i * 8].dt * 1000);
+      newCard.append($("<h4>").html(myDate.toLocaleDateString()));
+      var iconCode = response.list[i * 8].weather[0].icon;
+      var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+      newCard.append($("<img>").attr("src", iconURL));
+      newCard.append($("<p>").html("Temp: " + temp + "&#8457"));
+      newCard.append($("<p>").html("Humidity: " + humidity));
+        }
+      })
+  })
+};
+
+$("#searchCity").click(function() {
+  city = $("#city").val().trim();
+  getData();
+  var checkArray = searchHistory.includes(city);
+  if (checkArray == true) {
+    return
+  }
+  else {
+    searchHistory.push(city);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    var cityListButton = $("<a>").attr({
+        class: "list-group-item list-group-item-action",
+        href: "#"
+      });
+      cityListButton.text(city);
+      $(".list-group").append(cityListButton);
+  };
+});
